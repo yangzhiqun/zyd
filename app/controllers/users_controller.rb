8ad6858a126a05517a62451fd14c7f50b47965b4
ganggang <ganggang@ganggang.me>
2015-11-17 09:50:28 +0800

    修复更新用户导致的问题

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -146,11 +146,10 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.update_attributes(user_params)
-        flash[:notice] = "User #{@user.name} was successfully updated."
+        flash[:notice] = "User was successfully updated."
         format.html { redirect_to(:action => 'index') }
         format.xml { head :ok }
       else
-        logger.error @user.errors.as_json
         format.html { render :action => "edit" }
         format.xml { render :xml => @user.errors,
                             :status => :unprocessable_entity }
