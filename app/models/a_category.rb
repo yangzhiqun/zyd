2aa8fa74a1fb2af96160e70407388ed84fc32545
ganggang <ganggang@ganggang.me>
2015-11-20 19:57:02 +0800

    fix spsearch big category bug

--- app/models/a_category.rb
+++ app/models/a_category.rb
@@ -9,4 +9,29 @@ class ACategory < ActiveRecord::Base
   def rowspan
     DCategory.count(:conditions => ["a_category_id = ?", self.id])
   end
+
+  Category24 = %w{粮食及粮食制品
+    食用油、油脂及其制品
+    肉及肉制品
+    蛋及蛋制品
+    水产及水产制品
+    蔬菜及其制品
+    饮料
+    调味品
+    酒类
+    茶叶及其相关制品、咖啡
+    薯类及膨化食品
+    糖果及可可制品
+    炒货食品及坚果制品
+    豆及豆制品
+    蜂产品
+    冷冻饮品
+    乳制品
+    特殊膳食食品
+    食品添加剂
+    焙烤食品
+    罐头
+    食糖
+    水果及其制品
+  }
 end
