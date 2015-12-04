b20c4a49902c388bf41b6edc950f9390b2453344
ganggang <ganggang@ganggang.me>
2015-12-04 13:55:59 +0800

    rc

--- db/migrate/20151203125311_add_condition_to_tmp_sp_bsbs.rb
+++ db/migrate/20151203125311_add_condition_to_tmp_sp_bsbs.rb
@@ -1,9 +1,9 @@
 class AddConditionToTmpSpBsbs < ActiveRecord::Migration
   def change
-		add_column :tmp_sp_bsbs, :sp_s_215, :string unless column_exists?(:tmp_sp_bsbs, :sp_s_215)
-		add_column :tmp_sp_bsbs, :sp_s_68, :string unless column_exists?(:tmp_sp_bsbs, :sp_s_68)
-		add_column :tmp_sp_bsbs, :sp_s_13, :string unless column_exists?(:tmp_sp_bsbs, :sp_s_13)
-		add_column :tmp_sp_bsbs, :sp_s_27, :string unless column_exists?(:tmp_sp_bsbs, :sp_s_27)
+		add_column :tmp_sp_bsbs, :sp_s_215, :string
+		add_column :tmp_sp_bsbs, :sp_s_68, :string 
+		add_column :tmp_sp_bsbs, :sp_s_13, :string 
+		add_column :tmp_sp_bsbs, :sp_s_27, :string 
 
 		add_index :tmp_sp_bsbs, [:sp_s_215, :sp_s_68, :created_at, :sp_i_state], name: '215_68_created_at_state'
 		add_index :tmp_sp_bsbs, [:sp_s_13, :sp_i_state, :created_at], name: '13_state_created_at'
