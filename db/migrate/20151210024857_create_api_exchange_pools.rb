34c8ae07509879be57971ce6b8e2d33279737d85
ganggang <ganggang@ganggang.me>
2015-12-10 10:50:06 +0800

    添加 api exchange pool

---
+++ db/migrate/20151210024857_create_api_exchange_pools.rb
@@ -0,0 +1,11 @@
+class CreateApiExchangePools < ActiveRecord::Migration
+  def change
+    create_table :api_exchange_pools do |t|
+      t.integer :application_id, null: false
+      t.integer :sp_bsb_id, null: false
+      t.boolean :fetched, default: false
+
+      t.timestamps null: false
+    end
+  end
+end
