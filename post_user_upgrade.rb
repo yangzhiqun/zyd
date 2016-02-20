9de960b99d08bc236f23c5aca5562935798aeef1
刚刚 <ganggang@ganggang.me>
2016-02-20 22:36:20 +0800

    完善逻辑

--- post_user_upgrade.rb
+++ post_user_upgrade.rb
@@ -3,26 +3,36 @@
 require 'mysql2'
 require 'pp'
 
-CLIENT = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "123456", :database => "depot_demo_development")
+DB_CONFIG = {:host => "localhost", :username => "root", :password => "123456", :database => "depot_demo_development"}
 
+CLIENT = Mysql2::Client.new(DB_CONFIG)
+
+
+# 取出用户原tname与用户id的对应关系,放到 @user_mapper
 @user_mapper = {}
 CLIENT.query('select tname, id, uid from users').each do |user|
   @user_mapper[user['tname']] = [user['id'], user['uid']]
 end
 
-sp_bsbs_count = CLIENT.query('select count(1) as count from sp_bsbs').first['count']
-THREAD_SIZE = 100000
+# 取出spbsbs的总数
+sp_bsbs_count = CLIENT.query('select count(1) as count from sp_bsbs where user_id is null').first['count']
+
+# 每个线程的处理数量
+THREAD_SIZE = 50000
 
 threads = []
+# 开启总共的县城数量
 (sp_bsbs_count.to_f / THREAD_SIZE).ceil.times do |i|
   threads << Thread.new do
-    client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "123456", :database => "depot_demo_development")
+    client = Mysql2::Client.new(DB_CONFIG)
     puts "Thread #{i} started.\n"
 
-    client.query("select sp_s_16, sp_s_37, tname, id from sp_bsbs where user_id is null or uid is null limit #{THREAD_SIZE} offset #{i + THREAD_SIZE}").each_with_index do |row, index|
+    client.query("select sp_s_16, sp_s_37, tname, id from sp_bsbs where user_id is null limit #{THREAD_SIZE} offset #{(i + 1) * THREAD_SIZE}").each_with_index do |row, index|
       if @user_mapper.has_key?(row['tname'])
         user_id = @user_mapper[row['tname']]
-        sql = "update sp_bsbs set user_id = #{user_id[0]}, uid = '#{user_id[1]}', sp_s_37_user_id = #{user_id[0]} where id=#{row['id']}"
+        sql = "update sp_bsbs set user_id = #{user_id[0]}, sp_s_37_user_id = #{user_id[0]} where id=#{row['id']}"
+
+        client.query(sql)
 
         puts "T:#{i}, Updated: GC: #{row['sp_s_16']}, #{index}/#{THREAD_SIZE}\n#{sql}\n"
       else
@@ -34,10 +44,11 @@ threads = []
 end
 
 
+# 异议处理
 threads << Thread.new do
   puts 'SpYydjb Thread Started.'
-  client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "123456", :database => "depot_demo_development")
-  client.query('SELECT id, djr, blr, tbr, shr FROM sp_yydjbs').each do |row|
+  client = Mysql2::Client.new(DB_CONFIG)
+  client.query('SELECT id, djr, blr, tbr, shr FROM sp_yydjbs where djr_user_id IS NULL').each do |row|
     fields = {}
     fields[:djr_user_id] = @user_mapper[row['djr']][0] if @user_mapper.has_key?(row['djr'])
     fields[:blr_user_id] = @user_mapper[row['blr']][0] if @user_mapper.has_key?(row['blr'])
@@ -46,15 +57,19 @@ threads << Thread.new do
 
     unless fields.empty?
       sql = "UPDATE sp_yydjbs SET #{fields.map { |f, v| "#{f} = #{v}" }.join(',')} where id=#{row['id']}"
+
+      client.query(sql)
+
       puts "YYDJB: #{sql}\n"
     end
   end
 end
 
+# 核查处置
 threads << Thread.new do
   puts 'WtypCzb Thread Started.'
-  client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "123456", :database => "depot_demo_development")
-  client.query('SELECT id, blr, tbr, shr FROM wtyp_czb_parts').each do |row|
+  client = Mysql2::Client.new(DB_CONFIG)
+  client.query('SELECT id, blr, tbr, shr FROM wtyp_czb_parts where blr_user_id IS NULL AND tbr_user_id IS NULL AND shr_user_id IS NULL').each do |row|
     fields = {}
     fields[:blr_user_id] = @user_mapper[row['blr']][0] if @user_mapper.has_key?(row['blr'])
     fields[:tbr_user_id] = @user_mapper[row['tbr']][0] if @user_mapper.has_key?(row['tbr'])
@@ -62,6 +77,7 @@ threads << Thread.new do
 
     unless fields.empty?
       sql = "UPDATE wtyp_czb_parts SET #{fields.map { |f, v| "#{f} = #{v}" }.join(',')} where id=#{row['id']}"
+      client.query(sql)
       puts "WTYP_PARTS: #{sql}\n"
     end
   end
