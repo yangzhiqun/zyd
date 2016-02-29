918708b50ebf1ef4861b9552c8117b1bcbe83ed1
ganggang <ganggang@ganggang.me>
2016-02-29 20:04:29 +0800

    调整pad端更新接口逻辑

--- app/controllers/api/v1/client_controller.rb
+++ app/controllers/api/v1/client_controller.rb
@@ -2,10 +2,13 @@ class Api::V1::ClientController < ApplicationController
 	skip_before_filter :session_expiry, :verify_authenticity_token, :authorize
   skip_before_action :authenticate_user!
 
-	CURRENT_VERSION_CODE = 36
+	#CURRENT_VERSION_CODE = 40
   def version
+		config_path = Rails.root.join('config', 'app_version.json')
+		app_info = JSON.parse(File.read(config_path))
+
 		respond_to do |format|
-			format.json { render :json => {:status => 'OK', :msg => {:versionCode => CURRENT_VERSION_CODE}}}
+			format.json { render :json => {:status => 'OK', :msg => {:versionCode => app_info['version_code']}}}
 		end
   end
 
