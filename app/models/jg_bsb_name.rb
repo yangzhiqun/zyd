64f7f2dd2a6b771cb3bd03e2670e40807b220eb0
刚刚 <ganggang@ganggang.me>
2016-03-25 17:45:56 +0800

    完善机构自动清除缓存功能

--- app/models/jg_bsb_name.rb
+++ app/models/jg_bsb_name.rb
@@ -7,4 +7,10 @@ class JgBsbName < ActiveRecord::Base
   validates_presence_of :jg_bsb_id, :message => '机构id不可为空'
 
   belongs_to :jg_bsb
+
+  after_save :doing_after_update
+
+  def doing_after_update
+    `/bin/bash -l -c 'cd #{Rails.root.to_s} && bin/rake tmp:cache:clear RAILS_ENV=production'`
+  end
 end
