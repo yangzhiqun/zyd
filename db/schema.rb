528a4448620fb7d91120f2e6625990ad82a1b465
刚刚 <ganggang@ganggang.me>
2016-04-20 23:23:33 +0800

    修复输入营业执照号未带出省市县

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160415145643) do
+ActiveRecord::Schema.define(version: 20160420084650) do
 
   create_table "a_categories", force: :cascade do |t|
     t.integer  "bgfl_id",    limit: 4
