e0db8760dd5849354bc22d7af56a0311236c6737
root <405080310@qq.com>
2016-10-19 16:49:02 +0800

    同步bug

--- app/models/api/data_docking.rb
+++ app/models/api/data_docking.rb
@@ -6,10 +6,10 @@ class API::DataDocking
 	class << self
 		
 		# 用户对应Hash
-		User_data_hash = {"tname": "userName", "id_card": "userIdcardNum", "email": "userEmail", "state": "userStatus", "user_s_province": "", "user_code": "userIdCode", "mobile": "userPhone"}
+		User_data_hash = {:tname=>"userName",:id_card=>"userIdcardNum",:email=>"userEmail",:state=>"userStatus",:user_s_province=>"",:user_code=>"userIdCode",:mobile=>"userPhone"}
 			
 		# 机构对应Hash
-		Org_data_hash  = {"jg_name": "orgName", "jg_province": "orgName", "jg_code": "orgNumber"}		
+		Org_data_hash  = {:jg_name=>"orgName",:jg_province=>"orgName",:jg_code=>"orgNumber"}
 
 		# 凭证信息对应Hash
 		Credence_data_hash = {}
@@ -25,19 +25,19 @@ class API::DataDocking
 			end
 			crendence = user['credenceList'][0]
 			if crendence['credenceClass'].include?('002')
-				result["uid"] = crendence["loginName"]
+				result[:uid] = crendence["loginName"]
 			end
 			# 创建修时改开放用户权限
-			result["ca_user_status"] = 0
+			result[:ca_user_status] = 0
 
 			#--------------------------------
-			result["function_type"] = "2"
+			result[:function_type] = "2"
 			#result["jg_bsb_id"] = 10
 			jg_bsb = JgBsb.find_by_jg_code(user['orglist'][0]['orgNumber'])	
-			result["jg_bsb_id"] = jg_bsb.id unless jg_bsb.nil?
-			result["user_s_province"] = "吉林省"
+			result[:jg_bsb_id] = jg_bsb.id unless jg_bsb.nil?
+			result[:user_s_province] = "吉林省"
 			#result["mobile"] = "13657853565"
-			result["password"] = "12345678"
+			result[:password] = "12345678"
 			#--------------------------------
 
 			return result	
