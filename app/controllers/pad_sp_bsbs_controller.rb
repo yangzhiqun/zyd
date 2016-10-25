e8b2de61c31350f23b31ce477d8d0eca407962dd
root <root@localhost.zyd>
2016-10-25 18:56:06 +0800

    修改bug

--- app/controllers/pad_sp_bsbs_controller.rb
+++ app/controllers/pad_sp_bsbs_controller.rb
@@ -148,7 +148,7 @@ class PadSpBsbsController < ApplicationController
     # @sp_bsb.sp_s_37=session[:user_tname]
     @sp_bsb.sp_s_39=current_user.tel
     @sp_bsb.sp_s_52=current_user.user_s_province
-
+    @sp_bsb.sp_s_202 = current_user.user_s_province
     if params[:rwly].to_i != 0
       if params[:rwly].to_i == -1
         @sp_bsb.sp_s_2_1 = "#{SysConfig.get(SysConfig::Key::PROV)}食品药品监督管理局"
