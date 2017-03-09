a39be7d3291878c0ecffda126829606aa82ee072
root <root@localhost.zyd>
2017-03-09 17:43:18 +0800

    修改报送分类

--- app/controllers/baosong_as_controller.rb
+++ app/controllers/baosong_as_controller.rb
@@ -81,9 +81,8 @@ class BaosongAsController < ApplicationController
     @baosong_a.sheng = SysConfig.get(SysConfig::Key::PROV) 
     respond_to do |format|
       if !BaosongA.find_by_name(baosong_a_params["name"]).nil?
-        format.html {
-           redirect_to :back, :flash => {:error => "报送分类A名字重复！"}
-        }
+        flash[:error] = "报送分类A名字重复！"
+ 	format.html { render action: "new" }
       else
         if @baosong_a.save
           format.html { redirect_to @baosong_a, notice: 'Baosong a was successfully created.' }
@@ -106,9 +105,8 @@ class BaosongAsController < ApplicationController
     end
     respond_to do |format|
       if !BaosongA.find_by_name(baosong_a_params["name"]).nil? && @baosong_a.name != baosong_a_params["name"]
-        format.html {
-           redirect_to :back, :flash => {:error => "报送分类A名字重复！"}
-        }
+        flash[:error] = "报送分类A名字重复！"
+ 	format.html { render action: "new" }
       else
         if @baosong_a.update_attributes(baosong_a_params)
           format.html { redirect_to @baosong_a, notice: 'Baosong a was successfully updated.' }
