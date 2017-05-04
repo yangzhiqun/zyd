92d7d1d25a63296c0ccdce644b21986581e4401d
root <root@localhost.zyd>
2017-05-04 23:00:21 +0800

    bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -125,10 +125,12 @@ module ApplicationHelper
 			jg_type = current_user.jg_bsb.jg_type
 			super_jg = current_user.jg_bsb.jg_bsb_supers
 			super_jg.each{ |jg| jg_arr << jg.super_jg_bsb.jg_name}	
-			# 如果是监管部门显示自己
+			# 如果是监管部门显示自己跟上级局
 			if jg_type == 1
         jg_arr = []
 				jg_arr << current_user.jg_bsb.jg_name
+        super_jg = current_user.jg_bsb.jg_bsb_supers
+        super_jg.each{ |jg| jg_arr << jg.super_jg_bsb.jg_name}
 			end
 		end 
 		return jg_arr
