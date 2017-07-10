58772a21c73a7424f0e0f91c11ad6e00f602cd09
root <root@localhost.zyd>
2017-07-11 03:45:29 +0800

    genxin

--- db/migrate/20170703172304_change_column_to_check_items.rb
+++ db/migrate/20170703172304_change_column_to_check_items.rb
@@ -3,8 +3,8 @@ class ChangeColumnToCheckItems < ActiveRecord::Migration
     change_column :check_items, :BZFFJCXDW,:string, default: "/"
     change_column :check_items, :BZZXYXX,:string, default: "/"
     change_column :check_items, :BZZXYXXDW,:string, default: "/"
-    change_column :check_items, :JYYJJHB,:string, default: "/"
-    change_column :check_items, :BZ,:string, default: "/"
+    #change_column :check_items, :JYYJJHB,:string, default: "/"
+   # change_column :check_items, :BZ,:string, default: "/"
     change_column :check_items, :BZZDYXXDW, :string, default: "/"
   end
 end
